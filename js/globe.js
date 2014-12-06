4a71677f6c8f2015fb54f4b8120a2ced34bf6c87
cakesofwrath <dai.alan.dai@gmail.com>
2014-12-06 18:57:02 +0000

    Labelled data done, need to topojson it next

--- js/globe.js
+++ js/globe.js
@@ -3,50 +3,107 @@
 var CRUTData = []; //the actual weather/climate data as an ARRAY (the array in the data obj) - will only be temperature_anomaly
 var CRUTSchema = {}; //the schema obj received from the ncdump-json JSON- children objects latitude, longitued, time
 var lblCRUTData = {}; //obj of year arrays of objects
-d3.json('jsonData/data.json', function(err, data){
-//d3.json('jsonData/1850-2012.data.CRUT.json', function(err, data){
-    console.log('data receieved', data);
-    if(err)
-        console.log(err);
-    CRUTData = data.temperature_anomaly;
-    lblCRUTData = labelData(CRUTData, CRUTSchema); //make sure we call this AFTER we json this stuff (JS is async)
-    
-});
-//d3.json('jsonSchema/1850-2012.CRUT.json', function(err, data){
-d3.json('jsonSchema/schema.json', function(err, data){
-    console.log('schema receieved', data);
-    if(err)
-        console.log(err);
-    CRUTSchema = data;
-}) //Should functionize later
+function getUnSortData(){
+    d3.json('jsonData/data.json', function(err, data){
+    //d3.json('jsonData/1850-2012.data.CRUT.json', function(err, data){
+        console.log('data receieved', data);
+        if(err)
+            console.log(err);
+        CRUTData = data.temperature_anomaly;
+        lblCRUTData = labelData(CRUTData, CRUTSchema); //make sure we call this AFTER we json this stuff (JS is async)
+        //console.log(JSON.stringify(lblCRUTData));
+    });
+    //d3.json('jsonSchema/1850-2012.CRUT.json', function(err, data){
+    d3.json('jsonSchema/schema.json', function(err, data){
+        console.log('schema receieved', data);
+        if(err)
+            console.log(err);
+        CRUTSchema = data;
+    }) 
+} //This function shouldn't be needed bc I used it already to get teh data. Holy crap that sucked.
 
+function processLabelledData(data){
+    d3.json('avgLabelledData.json', function(err, data){
+        if(err)
+            console.log(err);
+    });
+}
 
 
 /*
     Takes the raw ncdumped data and schema jsons and returns an object of each year's data contained within an array of the data objects
+    This was used to produce avgLabelledData.json
 */
 function labelData(data, schema){
     var lblData = {};
     var currYear = schema.time[0].toString().substring(0, 4);
-    lblData[currYear] = [];
+    var currMonth = schema.time[0].toString().substring(5, 7);
+    lblData[currYear]={};
+    lblData[currYear][currMonth] = [];
     //latitude, then longitude, then time
     var indexInTempData = 0; //our pointer or whatever within the unlabelled data array
     console.log('before fors');
+ 
+    var avgData = {}; //object of years containing array of data objects with data. 
+    avgData[currYear]={};
+    //initial labelling
     for(var i in schema.time){
         for(var j in schema.longitude){
             for(var k in schema.latitude){ //0,4
                 if(schema.time[i].toString().substring(0,4) != currYear){
                     currYear = schema.time[i].toString().substring(0,4);
-                    lblData[currYear] = [];
+                    lblData[currYear] = {};
+                    avgData[currYear]={};
+                }
+                if(schema.time[i].toString().substring(5,7) != currMonth){
+                    currMonth = schema.time[i].toString().substring(5,7);
+                    lblData[currYear][currMonth] = [];
                 }
                 if(data[indexInTempData])
-                    lblData[currYear].push(new Data(schema.latitude[k], schema.longitude[j], schema.time[i], data[indexInTempData]));
+                    lblData[currYear][currMonth].push(new Data(schema.latitude[k], schema.longitude[j], schema.time[i], data[indexInTempData]));
                 indexInTempData++;
             }
         }
     }
-    console.log('done\n', lblData);
-    return lblData;
+
+    
+    console.log(avgData);
+    //averaging Data
+    for(var yr in lblData){
+        for(var loo in schema.longitude){
+            var loo1 = schema.longitude[loo];
+            //console.log(yr, loo1)
+            
+            avgData[yr][loo1] = [];
+            for(var laa in schema.latitude){
+                laa1 = schema.latitude[laa];
+                //console.log(yr, laa1, loo1);
+                //console.log(avgData[yr][loo1][laa1])
+                avgData[yr][loo1][laa1] = {sum: 0, avg: 0, ct: 0};
+            }
+        }
+        for(var m in lblData[yr]){
+            for(var d in lblData[yr][m]){
+                var laat = lblData[yr][m][d]["latitude"], 
+                    loong = lblData[yr][m][d]["longitude"];
+                
+                avgData[yr][loong][laat]['sum'] += lblData[yr][m][d]["data"];
+                avgData[yr][loong][laat]['ct']++;
+            }
+        }
+    }
+    for(var yrr in avgData){
+        for(var laaat in avgData[yrr]){
+            for(var looong in avgData[yrr][laaat]){
+                //console.log(avgData[yrr][laaat][looong])
+                avgData[yrr][laaat][looong]['avg'] = avgData[yrr][laaat][looong]['sum'] / avgData[yrr][laaat][looong]['ct'];
+                delete avgData[yrr][laaat][looong]['sum'];
+                delete avgData[yrr][laaat][looong]['ct'];
+            }
+        }
+    }
+    console.log('avg', avgData);
+    return avgData;
 }
 
 function Data(lat, long, tim, _data){
